e868d5488dd22cb86e24473aeeb3ad34434d92be
unknown <danielsafari143@gmail.com>
2024-01-22 21:46:09 +0100

    ADD CONTROLLERS

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,104 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using password.hashedpassword;
+using UserTasks.Models.Tasks;
+using UserTasks.task.tdo;
+using UserTasks.tasksServices;
+using UserTasks.UserServices;
+
+namespace UserTasks.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+
+    private TasksRepository repository;
+    private HashedPassword hashedPassword;
+    private UserRepository userRepository1;
+
+    public TaskController(TasksRepository repository, HashedPassword hashedPassword, UserRepository userRepository)
+    {
+        this.repository = repository;
+        this.hashedPassword = hashedPassword;
+        this.userRepository1 = userRepository;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<List<UserTask>> GetTasks()
+    {
+        return await repository.GetTasksAsync();
+    }
+
+    [HttpPost]
+    [Consumes("application/json")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> Post(TaskDTO taskDTO)
+    {
+        var task = new UserTask
+        {
+            Title = taskDTO.Title,
+            Description = taskDTO.Description,
+            UsersId = taskDTO.UserID,
+            EnDate = taskDTO.EndDate
+        };
+        try
+        {
+            await repository.CreateUserAsync(task);
+        }
+        catch
+        {
+
+            return NotFound(new { Message = "There is no user with this id", Id = taskDTO.UserID });
+        }
+        return CreatedAtAction("GetTasks", new { ID = task }, task);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<UserTask>> GetOneTask(int id)
+    {
+        try
+        {
+            UserTask task = await repository.findOne(id);
+            return task;
+        }
+        catch
+        {
+            return NotFound(new { Message = "There is no task with this id", Id = id });
+        }
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTask(int id)
+    {
+        try
+        {
+            UserTask user = await repository.delete(id);
+            return user == null ? NotFound() : NoContent();
+        }
+        catch
+        {
+            return NotFound(new { Message = "There is no task with this id", Id = id });
+        }
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<UserTask>> updated(int id, TaskDTO taskDTO)
+    {
+        try
+        {
+            UserTask task = await repository.update(id, taskDTO);
+            return task;
+        }
+        catch
+        {
+            return NotFound(new { Message = "There is no task with this id", Id = id });
+        }
+    }
+
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,105 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using password.hashedpassword;
+using UserTasks.Models.User;
+using UserTasks.user.tdo;
+using UserTasks.UserServices;
+
+namespace UserTasks.Controllers;
+
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+	private UserRepository repository;
+	private HashedPassword hashedPassword;
+
+	public UserController(UserRepository repository, HashedPassword hashedPassword)
+	{
+		this.repository = repository;
+		this.hashedPassword = hashedPassword;
+	}
+
+	[HttpGet]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	public async Task<List<Users>> GetUser()
+	{
+		return await repository.GetUsersAsync();
+	}
+
+	[HttpPost]
+	[AllowAnonymous]
+	[Consumes("application/json")]
+	[ProducesResponseType(StatusCodes.Status201Created)]
+	public async Task<IActionResult> Post(UserDTO userTDO)
+	{
+		var user = new Users
+		{
+			username = userTDO.Username,
+			Password = hashedPassword.hashedpassword(userTDO.Password),
+			email = userTDO.Email,
+			Tasks = (ICollection<Models.Tasks.UserTask>)userTDO.Tasks
+		};
+
+		try
+		{
+			await repository.GetUserAsync(userTDO.Email);
+		}
+		catch (InvalidOperationException e)
+		{
+			if (e.Message == "Sequence contains more than one element.")
+			{
+				return Conflict(new { Message = "Email already exists!" });
+			}
+			await repository.CreateUserAsync(user);
+			return CreatedAtAction("GetUser", new { id = user.ID }, user);
+		}
+		return Conflict(new { Message = "Email already exists!" });
+	}
+
+	[HttpGet("{id}")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	public async Task<ActionResult<Users>> GetOneUser(int id)
+	{
+		try
+		{
+			Users user = await repository.findOne(id);
+			return user;
+		}
+		catch
+		{
+			return NotFound(new { Message = "There is no user with this id", Id = id });
+		}
+	}
+
+	[HttpDelete("{id}")]
+	public async Task<IActionResult> DeleteUser(int id)
+	{
+		try
+		{
+			Users user = await repository.delete(id);
+			return Accepted(new { Message = "User deleted succefully" });
+		}
+		catch
+		{
+			return NotFound(new { Message = "There is no user with this id", id });
+		}
+	}
+
+	[HttpPut("{id}")]
+	public async Task<ActionResult<Users>> update(int id, Users users)
+	{
+		try
+		{
+			Users prUser = await repository.update(id, users);
+			return CreatedAtAction("GetOneUser", new { id = prUser.ID }, prUser);
+		}
+		catch
+		{
+			return NotFound(new { Message = "There is no user with this id", Id = id });
+		}
+	}
+}
